527416763927e2e718395050ae3dc92ad50b921f
Stas <sbamburov.exceedteam@gmail.com>
2022-03-17 16:30:49 +0300

    Added search with autocomplete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,43 @@ import Hall from "./Hall";
 import Statistic from "./Statistic";
 import seatArray from "../sample-hall";
 import Modal from "./Modal";
+import debounce from "lodash.debounce";
+import axios from "axios";
 
 const App = () => {
   const [seats, setSeat] = useState(seatArray);
   const [open, setOpen] = useState(false);
+  const [isEdit, setEdit] = useState(false);
   const [currentSeat, setCurrent] = useState({});
+  const [query, setQuery] = useState([]);
+  const [currentMovie, setCurrentMovie] = useState({
+    title: "Название фильма",
+    release_date: "",
+    backdrop_path: "",
+    overview: "",
+    poster_path: "",
+  });
+
+  const editName = () => {
+    setEdit(!isEdit);
+  };
+
+  const itemClickHandler = (key) => {
+    setCurrentMovie(query.find((movie) => movie.id === key));
+    setEdit(!isEdit);
+  };
+
+  const currentName = (event) => {
+    setCurrentMovie({ ...currentMovie, title: event.target.value });
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=9113d8d7a27ece3272cc84a40839bea3&query=${currentMovie.title}&language=ru-RU`
+      )
+      .then((response) => setQuery(response.data.results))
+      .catch((err) => setQuery([{ title: "Попробуйте иначе", id: 100002 }]));
+  };
+
+  const debouncedOnChange = debounce(currentName, 400);
 
   const reserveSeat = (key) => {
     setSeat(
@@ -48,8 +80,15 @@ const App = () => {
     );
   }, [currentSeat]);
 
+  const validatePrice = (price) => {
+    if (price < 0) return 0;
+    if (price > 999) return 999;
+    return +price;
+  };
+
   const editSeatPrice = (event) => {
-    setCurrent({ ...currentSeat, price: +event.target.value });
+    const price = validatePrice(event.target.value);
+    setCurrent({ ...currentSeat, price: price });
   };
 
   return (
@@ -60,14 +99,20 @@ const App = () => {
           date="20.03.2022"
           reserveSeat={reserveSeat}
           openModal={openModal}
+          editName={editName}
+          debouncedOnChange={debouncedOnChange}
+          itemClickHandler={itemClickHandler}
+          query={query}
+          isEdit={isEdit}
+          currentMovie={currentMovie}
         />
-        <Statistic seats={seats} />
+        <Statistic seats={seats} currentMovie={currentMovie} />
       </div>
       <Modal open={open} onClose={closeModal}>
         <p>Номер места: {currentSeat.seatNumber}</p>
         <p>Статус: {currentSeat.reserved ? "Забронировано" : "Свободно"}</p>
         <p>
-          Цена: 
+          Цена:
           <input
             type="number"
             value={currentSeat.price}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 
-const Header = ({ date }) => {
-  const [cinemaName, setCinemaName] = useState({
-    name: "Название фильма",
-    edit: false,
+const Header = ({
+  date,
+  editName,
+  debouncedOnChange,
+  itemClickHandler,
+  query,
+  isEdit,
+  currentMovie,
+}) => {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    if (isEdit) {
+      inputEl?.current?.focus();
+    }
   });
-
-  const editName = () => {
-    setCinemaName( { ...cinemaName, edit: !cinemaName.edit } );
-  };
-
-  const currentName = (event) => {
-    setCinemaName((prev) => ({ ...prev, name: event.target.value }));
-  };
 
   return (
     <div className="cinema-name">
       <div className="cinema-name__header">
         Фильм&nbsp;
-        {cinemaName.edit ? (
-          <input
-            type="text"
-            value={cinemaName.name}
-            onChange={currentName}
-            className="cinema-name__input"
-            maxLength={20}
-          ></input>
+        {isEdit ? (
+          <form className="search_form">
+            <input
+              type="text"
+              placeholder={currentMovie.title}
+              onChange={debouncedOnChange}
+              className="cinema-name__input"
+              maxLength={20}
+              ref={inputEl}
+            ></input>
+            <ul className="autocomplete">
+              {currentMovie.title && isEdit
+                ? query.map((movie) => {
+                    return (
+                      <li
+                        key={movie.id}
+                        className="autocomplete__item"
+                        onClick={() => itemClickHandler(movie.id)}
+                      >
+                        {movie.title}
+                        <span>
+                          {movie.release_date
+                            ? movie.release_date.substr(0, 4) + " г."
+                            : null}
+                        </span>
+                      </li>
+                    );
+                  })
+                : null}
+            </ul>
+          </form>
         ) : (
-          <span>"{cinemaName.name}"</span>
+          <span>"{currentMovie.title}"</span>
         )}
-        <button className={`cinema-name__edit_btn ${cinemaName.edit  ? 'active' : ''}`} onClick={editName}>
-          {cinemaName.edit ? "Сохранить" : "Изменить"}
+        <button
+          className={`cinema-name__edit_btn ${isEdit ? "active" : ""}`}
+          onClick={editName}
+        >
+          {isEdit ? "Сохранить" : "Изменить"}
         </button>
       </div>
       <div className="cinema-name__date">{date}</div>
